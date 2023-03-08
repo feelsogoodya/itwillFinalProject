091de02725c81ac9ccaf187430ab03bbe8679563
Doyeon557 <114053240+Doyeon557@users.noreply.github.com>
2023-03-08 17:04:05 +0900

    merge

--- Final_Project/src/main/java/com/itwillbs/controller/ProductController.java
+++ Final_Project/src/main/java/com/itwillbs/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.itwillbs.controller;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.itwillbs.domain.Cs_PageDTO;
 import com.itwillbs.domain.ProductDTO;
 import com.itwillbs.service.ProductService;
 
@@ -50,17 +52,14 @@ public class ProductController {
 	}
 	
 	@RequestMapping(value = "/product/insertForm", method = RequestMethod.GET)	
-	public String insertForm(Model model) {
+	public String insertForm() {
 		// 기본 이동방식 : 주소변경 없이 이동 
-		List<String> categories = productService.getCategories();
-		model.addAttribute("categories", categories);
 		return "product/insertForm";
 	}//
 
 	
 	@RequestMapping(value = "/product/insertPro", method = RequestMethod.POST)	
-	public String insertPro(HttpServletRequest request,
-							HttpSession session,
+	public String insertPro(HttpServletRequest request, 
 							MultipartFile file, 
 							MultipartFile file1, 
 							MultipartFile file2, 
@@ -94,87 +93,104 @@ public class ProductController {
 		
 		ProductDTO productDTO = new ProductDTO();
 		productDTO.setProductPic(resultFilename);
-		productDTO.setProductSeller((String)session.getAttribute("memId"));
 		productDTO.setProductTitle(request.getParameter("productTitle"));
 		productDTO.setProductCate(request.getParameter("productCate"));
 		productDTO.setProductContent(request.getParameter("productContent"));
 		productDTO.setProductGrade(request.getParameter("productGrade"));
 //		productDTO.setProductLoca(request.getParameter("productLoca"));
 		productDTO.setProductPrice(Integer.parseInt(request.getParameter("productPrice")));
-		System.out.println(resultFilename);
+		
 		productService.insertProduct(productDTO);
 		
 		
 		// 이동방식 : 주소변경 하면서 이동 
 		// response.sendRedirect() 이동
 		return "redirect:/notice/list";
+
+		
 	}//
 	
-	@RequestMapping(value = "/product/update", method = RequestMethod.GET)	
-	public String update(HttpServletRequest request,Model model) {
-		int num=Integer.parseInt(request.getParameter("productNum"));
+	@RequestMapping(value = "/product/list", method = RequestMethod.GET)
+	public String productList(Model model,HttpServletRequest request, HttpSession session) {
 		
-		ProductDTO dto=productService.getProduct(num);
-		
-		model.addAttribute("dto", dto);
-		
-		// 기본 이동방식 : 주소변경 없이 이동 
-		return "product/updateForm";
-	}//
-	
-	
-	@RequestMapping(value = "/product/updatePro", method = RequestMethod.POST)	
-	public String updatePro(HttpServletRequest request,
-							HttpSession session,
-							MultipartFile file, 
-							MultipartFile file1, 
-							MultipartFile file2, 
-							MultipartFile file3, 
-							MultipartFile file4, 
-							MultipartFile file5) throws Exception {
-		
-		System.out.println("ProductController updatePro() ");
-		
-		MultipartFile[] files = {file, file1, file2, file3, file4, file5};
-		String resultFilename = "";
-		for(MultipartFile f : files) {
-			
-			if(!f.getOriginalFilename().equals("")) {
-				UUID uuid =UUID.randomUUID();
-				String filename=uuid.toString()+"_"+f.getOriginalFilename();
-				FileCopyUtils.copy(f.getBytes(), new File(uploadPath, filename));
-				resultFilename += (filename + "|");
-			}
+		// 정렬 값 설정하기 기본값 최신순으로 
+		String filterValue = request.getParameter("filterValue");
+		if ("".equals(filterValue ) || filterValue == null) {
+			filterValue = "productDate";
 		}
-		resultFilename = resultFilename.substring(0, resultFilename.length() - 1);
-		System.out.println(resultFilename);
 		
-		ProductDTO productDTO = new ProductDTO();
-		productDTO.setProductNum(Integer.parseInt(request.getParameter("num")));
-		productDTO.setProductPic(resultFilename);
-		productDTO.setProductSeller((String)session.getAttribute("memId"));
-		productDTO.setProductTitle(request.getParameter("productTitle"));
-		productDTO.setProductCate(request.getParameter("productCate"));
-		productDTO.setProductContent(request.getParameter("productContent"));
-		productDTO.setProductGrade(request.getParameter("productGrade"));
-//		productDTO.setProductLoca(request.getParameter("productLoca"));
-		productDTO.setProductPrice(Integer.parseInt(request.getParameter("productPrice")));
-		productService.updateProduct(productDTO);
+
+
 		
-		// 이동방식 : 주소변경 하면서 이동 
-		// response.sendRedirect() 이동
-		return "redirect:/list/selllist";
 		
-	}//
+		// 카테고리 리스트 불러오기
+		List<Map<String, Object>> productCateList = productService.getproductCateList();
+		// 카테고리 리스트 불러오기
+		
+		
+		String startPrice = request.getParameter("startPrice");
+//		if ("".equals(startPrice ) || startPrice == null) {
+//			startPrice = "0";
+//		}
+		String endPrice = request.getParameter("endPrice");
+		System.out.println(startPrice + "~~~~~~"+endPrice);
+		
+		String productCate = request.getParameter("productCate");
+		String searchText = request.getParameter("searchText");
+		
+		String offset="0";
+		String limit="12";
+		
+		Map<String, String> params = new HashMap<>();
+		
+		params.put("memId", (String)session.getAttribute("memId"));
+		params.put("productCate", productCate);
+	    params.put("searchText", searchText);
+	    params.put("offset", offset);
+	    params.put("limit", limit);
+	    params.put("filterValue", filterValue);
+	    params.put("startPrice", startPrice);
+	    params.put("endPrice", endPrice);
+	    
+		
+
+		
+		List<Map<String, Object>> productList = productService.getProductList(params);
+		int productMax = productService.getproductMax(params);
+		System.out.println("productMax : "+productMax);
+		
+		
+		model.addAttribute("productCateList", productCateList);
+		model.addAttribute("productList", productList);
+		
+		
+		model.addAttribute("productCate", productCate);
+		model.addAttribute("searchText", searchText);
+		model.addAttribute("productMax", productMax);
+		model.addAttribute("filterValue", filterValue);
+		model.addAttribute("startPrice", startPrice);
+		model.addAttribute("endPrice", endPrice);
+		model.addAttribute("contextPath", request.getQueryString()); //데이터있을경우 화면 이동하게
+		
+		
+		
+		// 인기상품 리스트 
+		params.put("memId", null);
+		params.put("productCate", null);
+	    params.put("searchText", null);
+	    params.put("offset", "0");
+	    params.put("limit", "9");
+	    params.put("filterValue", "productDate");
+	    params.put("startPrice", null);
+	    params.put("endPrice", null);
+	    List<Map<String, Object>> popularProductList = productService.getProductList(params);
+		model.addAttribute("popularProductList", popularProductList);
+		
+		return "product/list";
+	}
 	
-	@RequestMapping(value = "/product/delete", method = RequestMethod.GET)	
-	public String delete(HttpServletRequest request) {
-		int productNum=Integer.parseInt(request.getParameter("productNum"));
-		
-		productService.deleteProduct(productNum);
-		
-		
-		return "redirect:/list/selllist";
-	}//
+	
+	
+	
 	
 }
